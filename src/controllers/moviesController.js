03e5ee6116ad7960b067ca4e3082edf82cc0f902
WaelS87 <wfakhrji@gmail.com>
2022-09-19 15:48:57 -0300

    nuevas models

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -3,8 +3,45 @@ module.exports = {
     list: (req, res) => {
         db.Movie.findAll()
             .then((movies)=>{
-                return res.send(movies)
+                return res.render('moviesList',{
+                    movies
+                })
             })
             .catch((err)=>console.log(err))
-    }
+    },
+    detail:(req,res)=>{
+        db.Movie.findByPk(req.params.id)
+        .then((movie)=>{
+            return res.render('moviesDetail',{
+                movie
+            })
+        })
+        .catch((err)=>console.log(err))
+    },
+    new:(req,res)=>{
+        db.Movie.findAll({
+            order:[
+                ['release_date','DESC']
+            ]
+        }).then((movies) =>{
+        return res.render('newestMovies',{
+            movies
+        })
+    })
+    .catch((err)=>console.log(err))
+   },
+   recommended:(req,res)=>{
+    db.Movie.findAll({
+        order:[
+            ['rating','DESC']
+        ],
+        limit:5
+    }).then((movies)=>{
+        return res.render('recommendedMovies',{
+            movies
+
+        })
+    })
+
+   }
 }
